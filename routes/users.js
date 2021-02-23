ebcf3dabc7b5c231729e21b85d901155b36d8e02
Craig Holliday <hello@craigaholliday.com>
2021-02-22 18:17:19 -0700

    small updates

--- routes/users.js
+++ routes/users.js
@@ -5,33 +5,33 @@ import { getAvailableStates } from '../services/covid-data.service';
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', async function (req, res, next) {
-  const allUsers = await getAllUsers();
-  res.json({ users: allUsers });
-});
+// /* GET users listing. */
+// router.get('/', async function (req, res, next) {
+//   const allUsers = await getAllUsers();
+//   res.json({ users: allUsers });
+// });
 
-/* POST create user. */
-router.post('/', async function (req, res, next) {
-  const { states } = getAvailableStates();
-  const { email, state } = req.body;
-  const stateJSON = JSON.parse(state);
-  const { name: stateName, abbreviation: stateAbbreviation } = stateJSON;
-  if (!email || !stateName || !stateAbbreviation) {
-    res.status(400);
-    res.end();
-    return;
-  }
-  try {
-    await createUser({ email, stateName, stateAbbreviation });
-    res.redirect('/success');
-  } catch (error) {
-    res.render('index', {
-      title: 'Covid-19 Email Updates',
-      states,
-      error: error.message,
-    });
-  }
-});
+// /* POST create user. */
+// router.post('/', async function (req, res, next) {
+//   const { states } = getAvailableStates();
+//   const { email, state } = req.body;
+//   const stateJSON = JSON.parse(state);
+//   const { name: stateName, abbreviation: stateAbbreviation } = stateJSON;
+//   if (!email || !stateName || !stateAbbreviation) {
+//     res.status(400);
+//     res.end();
+//     return;
+//   }
+//   try {
+//     await createUser({ email, stateName, stateAbbreviation });
+//     res.redirect('/success');
+//   } catch (error) {
+//     res.render('index', {
+//       title: 'Covid-19 Email Updates',
+//       states,
+//       error: error.message,
+//     });
+//   }
+// });
 
 module.exports = router;
