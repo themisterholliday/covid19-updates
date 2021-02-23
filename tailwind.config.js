c6a4c1f7d1b74101115b988253b39a32812d7e22
Craig Holliday <hello@craigaholliday.com>
2021-02-22 21:23:19 -0700

    fix purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const tailwindcssforms = require('@tailwindcss/forms');
 
 module.exports = {
-  purge: ['./**/*.{js,jsx,ts,tsx,html}'],
+  purge: ['./**/*.{js,jsx,ts,tsx,html,pug}'],
   darkMode: false, // or 'media' or 'class'
   variants: {
     extend: {},
